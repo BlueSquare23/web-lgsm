259a6eedb9f5cd51153d4b7bfa3583b6e5e31c02
John R <johnlradford23@gmail.com>
2023-03-20 21:09:01 -0400

    Add home page content
    * Started on install and add routes

--- app/models.py
+++ app/models.py
@@ -6,3 +6,11 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+
+class GameServer(db.Model):
+    id =  db.Column(db.Integer, primary_key=True)
+    # User supplied unique name.
+    install_name = db.Column(db.String(150), unique=True)
+    install_path = db.Column(db.String(150))
+    # The name of the lgsm game server script. For example, 'gmodserver'.
+    script_name = db.Column(db.String(150))
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash
-from .models import User
+from .models import User, GameServer
 from . import db
 
 views = Blueprint("views", __name__)
@@ -12,6 +12,19 @@ views = Blueprint("views", __name__)
 @views.route("/home", methods=['GET'])
 @login_required
 def home():
+    installed_servers = []
+    return render_template("home.html", user=current_user, installed_servers=installed_servers)
 
-    return render_template("home.html", user=current_user)
+######### Install Page #########
 
+@views.route("/install", methods=['GET'])
+@login_required
+def install():
+    return render_template("install.html", user=current_user)
+
+######### Add Page #########
+
+@views.route("/add", methods=['GET'])
+@login_required
+def add():
+    return render_template("add.html", user=current_user)

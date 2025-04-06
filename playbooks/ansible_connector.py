BlueSquare23/web-lgsm

commit e4f649805d99bdc6a76e001e46974ed348221e46

Make install.sh install certain important files to system level dirs
* For security reasons, I've decided the venv, ansible connector script, and
  playbooks need to be installed as root in system level directories.

* New Dirs:
  - `venv` -> `/opt/web-lgsm`
  - `playbooks` -> `/usr/local/share/web-lgsm`
  - `ansible_connector.py` -> `/usr/local/bin`

* Also tweaked install playbook to just download fresh `linuxgsm.sh` everytime.

* Also added new `uninstall.sh` script to undo the install when users want to
  remove it.

playbooks/ansible_connector.py
@@ -17,16 +17,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 ## Globals.
-# Plabook dir path.
-SCRIPTPATH = os.path.dirname(os.path.abspath(__file__))
-os.chdir(os.path.join(SCRIPTPATH, ".."))
-CWD = os.getcwd()
-JSON_VARS_FILE = os.path.join(CWD, "json/ansible_vars.json")
 PS = '/usr/bin/ps'
 PKILL = '/usr/bin/pkill'
+PLAYBOOKS = '/usr/local/share/web-lgsm'  # Playbook dir path.
+VENV = '/opt/web-lgsm'  # System venv path.
+APP_PATH = '/home/blue/Projects/web-lgsm'
 
-# Use cwd to import db classes from app.
-sys.path.append(CWD)
+# Import db classes from app.
+sys.path.append(APP_PATH)
 from app import db
 from app.models import User, GameServer
 
@@ -75,7 +73,7 @@ def db_get(server_id):
 
 def validate_username(username):
     """Checks supplied username is in accepted usernames."""
-    yaml_file_path = os.path.join(CWD, "playbooks/vars/accepted_usernames.yml")
+    yaml_file_path = os.path.join(PLAYBOOKS, "playbooks/vars/accepted_usernames.yml")
 
     with open(yaml_file_path, "r") as file:
         data = yaml.safe_load(file)
@@ -193,9 +191,9 @@ def run_install_new_game_server(server_id):
         print("Installation for server already completed!")
         exit(123)
 
-    ansible_cmd_path = os.path.join(CWD, "venv/bin/ansible-playbook")
+    ansible_cmd_path = os.path.join(VENV, "bin/ansible-playbook")
     install_gs_playbook_path = os.path.join(
-        CWD, "playbooks/install_new_game_server.yml"
+        PLAYBOOKS, "playbooks/install_new_game_server.yml"
     )
 
     sudo_pre_cmd = ["/usr/bin/sudo", "-n"]
@@ -302,7 +300,7 @@ def cancel_install(pid):
     """
     pid_cmd = get_script_cmd_from_pid(pid)
     self_path = os.path.join(SCRIPTPATH, __file__)
-    self_venv = os.path.join(CWD, 'venv/bin/python')
+    self_venv = os.path.join(VENV, 'bin/python')
     self_cmd = f"/usr/bin/sudo -n {self_venv} {self_path}"
 
     # Validate to ensure only killing instances of own script pid.
@@ -326,8 +324,8 @@ def run_delete_user(server_id):
 
     validate_username(server.username)
 
-    ansible_cmd_path = os.path.join(CWD, "venv/bin/ansible-playbook")
-    del_user_path = os.path.join(CWD, "playbooks/delete_user.yml")
+    ansible_cmd_path = os.path.join(VENV, "bin/ansible-playbook")
+    del_user_path = os.path.join(PLAYBOOKS, "playbooks/delete_user.yml")
     cmd = [
         "/usr/bin/sudo",
         "-n",

app/utils.py
@@ -30,7 +30,7 @@ from .cmd_descriptor import CmdDescriptor
 # Constants.
 CWD = os.getcwd()
 USER = getpass.getuser()
-ANSIBLE_CONNECTOR = os.path.join(CWD, "playbooks/ansible_connector.py")
+ANSIBLE_CONNECTOR = "/usr/local/bin/ansible_connector.py"
 PATHS = {
     "docker": "/usr/bin/docker",
     "sudo": "/usr/bin/sudo",
@@ -44,7 +44,7 @@ PATHS = {
 CONNECTOR_CMD = [
     PATHS["sudo"],
     "-n",
-    os.path.join(CWD, "venv/bin/python"),
+    "/opt/web-lgsm/bin/python",
     ANSIBLE_CONNECTOR,
 ]
 

app/views.py
@@ -35,7 +35,8 @@ from .proc_info_vessel import ProcInfoVessel
 # Constants.
 CWD = os.getcwd()
 USER = getpass.getuser()
-ANSIBLE_CONNECTOR = os.path.join(CWD, "playbooks/ansible_connector.py")
+VENV = "/opt/web-lgsm/"
+ANSIBLE_CONNECTOR = "/usr/local/bin/ansible_connector.py"
 PATHS = {
     "docker": "/usr/bin/docker",
     "sudo": "/usr/bin/sudo",
@@ -444,7 +445,7 @@ def install():
         cmd = [
             PATHS["sudo"],
             "-n",
-            os.path.join(CWD, "venv/bin/python"),
+            os.path.join(VENV, "bin/python"),
             ANSIBLE_CONNECTOR,
             "--install",
             str(server_id),

install.sh
@@ -6,12 +6,18 @@
 # Strict mode, close on any non-zero exit status.
 set -eo pipefail
 
-# In case script is invokes from outside of web-lgsm dir.
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTPATH
-
 # Debug mode.
 [[ $1 =~ '-d' ]] && set -x && echo $EUID && ls -lah && printenv
+
+## Globals
+# In case script is invokes from outside of web-lgsm dir.
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+VENV_PATH="/opt/web-lgsm"
+CONNECTOR_PATH="/usr/local/bin"
+PLAYBOOKS_PATH="/usr/local/share/web-lgsm/playbooks"
+
+cd $SCRIPTPATH
+sudo mkdir -p $PLAYBOOKS_PATH
 
 # Colors!!!
 red="\001\e[31m\002"
@@ -146,31 +152,33 @@ echo "$all_reqs" >> 'apt-reqs.txt'
 for req in $(cat 'apt-reqs.txt'); do
     if ! sudo dpkg -l | grep -w "$req" &>/dev/null; then
         echo -e "${green}####### Installing \`$req\`...${reset}"
+        echo $req >> installed.log
         sudo apt-get install -y $req
     fi
 done
 
 if ! which python3  &>/dev/null; then
     echo -e "${green}####### Installing \`python3\`...${reset}"
+    echo "python3" >> installed.log
     sudo apt-get install -y python3
 fi
 
 if ! which pip3 &>/dev/null; then
     echo -e "${green}####### Installing \`pip3\`...${reset}"
+    echo "python3-pip" >> installed.log
     sudo apt-get install -y python3-pip
 fi
 
 echo -e "${green}####### Setting up Virtual Env...${reset}"
-python3 -m venv venv
-source venv/bin/activate
+sudo python3 -m venv $VENV_PATH
 
 if [[ $sys_name =~ Ubuntu ]]; then
     echo -e "${green}####### Upgrading \`pip3\`...${reset}"
-    python3 -m pip install --upgrade pip
+    sudo $VENV_PATH/bin/python3 -m pip install --upgrade pip
 fi
 
 echo -e "${green}####### Installing Python Requirements...${reset}"
-python3 -m pip install -r requirements.txt
+sudo $VENV_PATH/bin/python3 -m pip install -r requirements.txt
 
 echo -e "${green}####### Installing NPM Requirements...${reset}"
 cd $SCRIPTPATH/app/static/js
@@ -178,21 +186,29 @@ npm install @xterm/xterm
 npm install --save @xterm/addon-fit
 cd $SCRIPTPATH
 
-# Setup sudoers rules to allow web-lgsm to run multi game server user
-# management components without needing to prompt for a sudo pass.
+## Install Ansible Connector & Playbook files.
+echo -e "${green}####### Installing Web-LGSM Ansible Connector...${reset}"
+
+# First hardcode web-lgsm system user into accepted_users validation list and
+# web_user ansible vars files.
+echo "  - $USER" >> $SCRIPTPATH/playbooks/vars/accepted_usernames.yml
+echo "web_lgsm_user: $USER" > $SCRIPTPATH/playbooks/vars/web_lgsm_user.yml
+
+# Hardcode APP_PATH in connector script.
+sed -i "s#APP_PATH = ''#APP_PATH = '$SCRIPTPATH'#g" $SCRIPTPATH/playbooks/ansible_connector.py
+
+# Then copy those files into system dirs.
+sudo cp -r playbooks/* $PLAYBOOKS_PATH
+sudo mv $PLAYBOOKS_PATH/ansible_connector.py $CONNECTOR_PATH
+
 echo -e "${green}####### Setting up Sudoers Rules...${reset}"
 
-apb="$SCRIPTPATH/venv/bin/ansible-playbook"
-venv_python="$SCRIPTPATH/venv/bin/python"
-ansible_connector="$SCRIPTPATH/playbooks/ansible_connector.py"
-accpt_usernames="$SCRIPTPATH/playbooks/vars/accepted_usernames.yml"
-web_lgsm_user_vars="$SCRIPTPATH/playbooks/vars/web_lgsm_user.yml"
+apb="$VENV_PATH/bin/ansible-playbook"
+venv_python="$VENV_PATH/bin/python"
+ansible_connector="$CONNECTOR_PATH/ansible_connector.py"
+accpt_usernames="$PLAYBOOKS_PATH/vars/accepted_usernames.yml"
+web_lgsm_user_vars="$PLAYBOOKS_PATH/vars/web_lgsm_user.yml"
 sudoers_file="/etc/sudoers.d/$USER-$USER"
-
-# Hardcode web-lgsm system user into accepted_users validation list and
-# web_user ansible vars files.
-echo "  - $USER" >> $accpt_usernames
-echo "web_lgsm_user: $USER" > $web_lgsm_user_vars
 
 # Write sudoers rule for passwordless install & delete.
 sudoers_rule="$USER ALL=(root) NOPASSWD: $venv_python $ansible_connector *"
@@ -204,14 +220,9 @@ sudo visudo -cf "$temp_sudoers"  # Validate new file.
 sudo mv "$temp_sudoers" "$sudoers_file"
 
 # Lock playbook files down for security reasons.
-sudo find $SCRIPTPATH/playbooks -type f -exec chmod 644 {} \;
-sudo find $SCRIPTPATH/playbooks -type d -exec chmod 755 {} \;
+sudo find $PLAYBOOKS_PATH -type f -exec chmod 644 {} \;
+sudo find $PLAYBOOKS_PATH -type d -exec chmod 755 {} \;
 sudo chmod 755 $apb $ansible_connector
-sudo chown -R root:root $apb "$SCRIPTPATH/playbooks"
-# Can't make files immutable in containers.
-if [[ -z $CONTAINER ]]; then
-    sudo chattr +i $apb $ansible_connector
-fi
 
 # Finally setup random key.
 random_key=$(echo $RANDOM | md5sum | head -c 20)

playbooks/install_new_game_server.yml
@@ -29,6 +29,14 @@
         - tasks/create_new_user.yml
         - tasks/set_default_new_user_group.yml
 
+    - name: Download linuxgsm.sh to /tmp/
+      get_url:
+        url: https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/linuxgsm.sh
+        dest: /tmp/linuxgsm.sh
+        mode: '0755'
+        owner: "{{ username }}"
+        group: "{{ username }}"
+
     - name: Create new game server directory
       file:
         path: "{{ server_install_path }}"
@@ -39,30 +47,37 @@
 
     - name: Use linuxgsm.sh to fetch {{ server_script_name }}
       command:
-        chdir: "{{ playbook_dir }}/../scripts"
+        chdir: /tmp
         argv:
           - ./linuxgsm.sh 
           - "{{ server_script_name }}"
       register: lgsmsh_result
-      become: yes
+      become: yes 
       become_user: "{{ web_lgsm_user }}"
 
     - name: Print linuxgsm.sh output
       debug:
         msg: "Output: {{ lgsmsh_result.stdout }}"
 
-    - name: Copy {{ server_script_name }} into new server dir
+    - name: Copy {{ server_script_name }} into new server dir 
       copy:
-        src: "{{ playbook_dir }}/../scripts/{{ server_script_name }}"
+        src: "/tmp/{{ server_script_name }}"
         dest: "{{ server_install_path }}"
         owner: "{{ username }}"
         group: "{{ username }}"
         mode: '0750'
 
-    - name: Delete {{ server_script_name }} file
-      file:
-        path: "{{ playbook_dir }}/../scripts/{{ server_script_name }}"
-        state: absent
+    - name: Clean up temporary files
+      block:
+        - name: Remove downloaded linuxgsm.sh
+          file:
+            path: /tmp/linuxgsm.sh
+            state: absent
+
+        - name: Remove generated {{ server_script_name }}
+          file:
+            path: "/tmp/{{ server_script_name }}"
+            state: absent
 
     - name: Create ~/.ssh directory exists and set perms
       file:

uninstall.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+# Uninstalls the web-lgsm base web application system files.
+
+# Colors!!!
+red="\001\e[31m\002"
+green="\001\e[32m\002"
+reset="\001\e[0m\002"
+
+# Could break system python if pip is run as root.
+if [[ "$EUID" -eq 0 ]]; then
+    echo -e "${red}Don't run this script as root!${reset}"
+    exit 1
+fi
+
+sudo -v
+
+## Debug / verbose mode.
+[[ $1 =~ '-d' ]] && set -x
+
+sudo rm -rf /opt/web-lgsm
+sudo rm /usr/local/bin/ansible_connector.py
+sudo rm -rf /usr/local/share/web-lgsm
+sudo rm /etc/sudoers.d/$USER-$USER
+
+set +x 
+
+echo -e "$green ✓ $reset Web-LGSM System Components Removed!"
+echo "Simply remove the web-lgsm directory to complete the uninstall."

web-lgsm.py
@@ -55,16 +55,14 @@ def run_command_popen(command):
 
 def relaunch_in_venv():
     """Activate the virtual environment and relaunch the script."""
-    venv_path = os.path.join(SCRIPTPATH, "venv/bin/activate")
+    venv_path = "/opt/web-lgsm/bin/activate"
     if not os.path.isfile(venv_path):
         err_msg = f"""\
  [!] Virtual environment not found at {venv_path}
  [*] Create a virtual environment using the following commands:
-         cd {SCRIPTPATH}
-         python3 -m venv venv
+         sudo python3 -m venv /opt/web-lgsm
  [*] Then install the required pip packages with this command:
-        source venv/bin/activate
-        pip3 install -r requirements.txt\
+        sudo /opt/web-lgsm/bin/pip3 install -r requirements.txt\
         """
         exit(err_msg)
 

commit 4f421bc4b71451837019652656c079d0834119aa

Correctly reset APP_PATH var in ansible connector
* Realized why the tests were failing originally after:
  e4f649805d99bdc6a76e001e46974ed348221e46
 - It was the APP_PATH var I created in the connector.
 - When I moved connector to /usr/local/bin/ forgot to update APP_PATH so it
   could be replaced by install.sh.
 - Now :fingers_crossed: things should fire off without a hitch...

playbooks/ansible_connector.py
@@ -21,7 +21,7 @@ PS = '/usr/bin/ps'
 PKILL = '/usr/bin/pkill'
 PLAYBOOKS = '/usr/local/share/web-lgsm'  # Playbook dir path.
 VENV = '/opt/web-lgsm'  # System venv path.
-APP_PATH = '/home/blue/Projects/web-lgsm'
+APP_PATH = ''  # <-- TO ME: REMEMBER TO MAKE EMPTY STRING AGAIN WHEN THIS SCRIPT GET'S UPDATED!
 
 # Import db classes from app.
 sys.path.append(APP_PATH)
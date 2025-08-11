c953b940d068b1aeea1765d7ff08f0944758a6dc
Johnny <johnlradford23@gmail.com>
2025-08-10 21:10:35 -0400

    Fix docker install step
    * Just cleaned up the command really and it should work now.
      - Have to push from dev before I can test on my debian machine.

--- install.sh
+++ install.sh
@@ -115,31 +115,30 @@ function install_docker() {
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
 
+    codename=$(. /etc/os-release && echo "$VERSION_CODENAME")
+
     if [[ ! $SYS_NAME =~ Ubuntu ]]; then
         # Add Docker's official GPG key:
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
         
         # Add the repository to Apt sources:
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $codename stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     else
         # Else do Debian install.
         sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
         
         # Add the repository to Apt sources:
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $codename stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     fi
 
     sudo apt-get update
 
     echo -e "${GREEN}####### Docker install completed successfully!${RESET}"
+    printf "Run: ./docker-setup.py --add\n  to add your game server port and build confs\n"
     exit
 }
 
